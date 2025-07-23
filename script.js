65d4cd7bc8e7b334faf5e3101e282815792864a6
Arun <arundevweb7@outlook.com>
2025-07-23 19:28:59 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function toggleDarkMode() {
-  const body = document.body;
-  const button = document.getElementById("darkModeBtn");
 
-  body.classList.toggle("dark-mode");
-
-  if (body.classList.contains("dark-mode")) {
-    button.textContent = "Light Mode";
-  } else {
-    button.textContent = "Dark Mode";
-  }
-}
 
 
 
