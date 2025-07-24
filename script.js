0935d8b45a9983b7f90df3e30acab14ce2dabd53
Arun <arundevweb7@outlook.com>
2025-07-24 20:45:11 +0530

    Updated: updated image loading

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ window.onload = function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
  
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTopButton");
 
-  
+  scrollToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  window.addEventListener("scroll", function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+  });
+});
+
